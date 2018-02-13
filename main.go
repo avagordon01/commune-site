8d46770585548ae36e39caf8bf3f151cc504afc5
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-02-13 21:08:26 +0000

    merge wrapper and base template

--- main.go
+++ main.go
@@ -31,10 +31,9 @@ func main() {
 	templates = make(map[string]*template.Template)
 	func_map := template.FuncMap{"human_time": humanize.Time}
 	templates["base"] = template.Must(template.ParseFiles("templates/base.html")).Funcs(func_map)
-	templates["wrapper"] = template.Must(template.Must(templates["base"].Clone()).ParseFiles("templates/wrapper.html"))
-	templates["home"] = template.Must(template.Must(templates["wrapper"].Clone()).ParseFiles("templates/home.html"))
-	templates["post"] = template.Must(template.Must(templates["wrapper"].Clone()).ParseFiles("templates/post.html"))
-	templates["search"] = template.Must(template.Must(templates["wrapper"].Clone()).ParseFiles("templates/search.html"))
+	templates["home"] = template.Must(template.Must(templates["base"].Clone()).ParseFiles("templates/home.html"))
+	templates["post"] = template.Must(template.Must(templates["base"].Clone()).ParseFiles("templates/post.html"))
+	templates["search"] = template.Must(template.Must(templates["base"].Clone()).ParseFiles("templates/search.html"))
 	templates["preview"] = template.Must(template.Must(templates["base"].Clone()).ParseFiles("templates/preview.html"))
 
 	mux := http.NewServeMux()
