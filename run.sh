8c4e91c281732f8bba5d995a0927a0bfcf3e1696
Patrick Gordon <pgpatrickgordon@gmail.com>
2018-02-14 14:13:33 +0000

    improve run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,12 @@
+#for a quicker development loop
+#visudo to allow that specific sudo command without a password
+#call run.sh in a while loop `while ./run.sh; do true; done`
+#then press C-c to restart the server easily
+
 set -e
 
-GOPATH=$(go env GOPATH)
+SRV_BIN=$(go env GOPATH)/bin/site
 
 go install
-sudo /usr/bin/setcap cap_net_bind_service=+ep $GOPATH/bin/site
-$GOPATH/bin/site
+sudo /usr/bin/setcap cap_net_bind_service=+ep $SRV_BIN
+exec $SRV_BIN
